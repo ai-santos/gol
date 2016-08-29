5b15d7a3aafceae50b0db4e6464180694f236822
sydroth <sydrothman@gmail.com>
2016-08-29 14:47:25 -0700

    can view sample sign-up form

--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 import express from 'express'
+import routes from './routes'
+import pug from 'pug'
+import path from 'path'
 
 const server = express()
 
+server.listen(process.env.PORT || 3000)
+
+//view engine setup
+server.set('views', path.join(__dirname, 'views'))
+server.set('view engine', 'pug')
+
+//user routes
+server.use('/', routes)
 server.get('/', (req,res) => {
   res.send('hello auto deployed world')
 })
-
-
-server.listen(process.env.PORT || 3000)
