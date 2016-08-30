09f6285a35ce03fbdac36360ac6aceed702cd10b
Aileen Santos <asantos3026@gmail.com>
2016-08-30 12:08:52 -0700

    users can log in, sessions assign userIds

--- src/database.js
+++ src/database.js
@@ -2,3 +2,24 @@ const databaseName = 'goldb'
 const connectionString = `postgres://${process.env.USER}@localhost:5432/${databaseName}`
 const pgp = require('pg-promise')()
 const db = pgp(connectionString)
+
+const createUser = function (attributes) {
+  const sql = `
+    INSERT INTO
+      users (email, encrypted_password, created_at)
+    VALUES
+      ($1, $2, now())
+    RETURNING
+      *
+  `
+  const variables = [
+    attributes.email,
+    attributes.password
+  ]
+
+  return db.one(sql, variables)
+}
+
+export default { 
+  createUser
+}
--- src/server.js
+++ src/server.js
@@ -4,18 +4,26 @@ import pug from 'pug'
 import path from 'path'
 import bodyParser from 'body-parser'
 import session from 'express-session'
+import logger from 'morgan'
+import serveFavicon from 'serve-favicon'
 
 const server = express()
-
 
 //view engine setup
 server.set('views', path.join(__dirname, 'views'))
 server.set('view engine', 'pug')
 
 // middleware
+server.use(logger('dev'))
+server.use(bodyParser.json())
 server.use(express.static(__dirname+'/public'))
 server.use(bodyParser.urlencoded({ extended: true }))
-server.use(session({resave: true, saveUninitialized: true, secret: 'SOMERANDOMSECRETHERE', cookie: { maxAge: 6000}}))
+server.use(session({
+  resave: true, 
+  saveUninitialized: true, 
+  secret: 'SOMERANDOMSECRETHERE', 
+  cookie: { maxAge: 6000}
+}))
 
 
 // routes
